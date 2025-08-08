e1737f891157a5e717c723fd0ff0d065a8491eb3
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-08 22:41:48 +0400

    create review model

---
+++ Models/Review.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoom_Architecture_DataAnnotation_FluentAPI.Models
+{
+    public class Review
+    {
+        // Review class representing a hotel room review with properties for ID, rating, comment, and associated reservation.
+        public int ReviewId { get; set; }
+        public int Rating { get; set; } // Rating out of 5
+        public string Comment { get; set; } = default!;
+    }
+}
