91cfdb274a36755877cd2e4a71d7b0fa45cc46cf
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-09 00:08:34 +0400

    navigation property to reservations for room model

--- Models/Room.cs
+++ Models/Room.cs
@@ -3,22 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace HotelRoom_Architecture_DataAnnotation_FluentAPI.Models
 {
     public class Room
     {
         // Room class representing a hotel room with properties for ID, room number, daily rate, and reservation status.
-        [key]
-        public int Id { get; set; }
+        public int RoomId { get; set; }
 
-        [Required]
-        public int RoomNumber { get; set; } // Unique
-
-        [Range(100, double.MaxValue)]
         public decimal DailyRate { get; set; }
 
-        [Required]
-        public bool IsReserved { get; set; } 
+        public bool IsReserved { get; set; }
+
+        public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>(); // navigation property to reservations
+
     }
 }
