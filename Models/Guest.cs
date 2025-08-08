9f2b48ff491005b787be4169efc6f5b89d629e7f
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-08 15:24:56 +0400

    guest class with attributes and data annotaion

---
+++ Models/Guest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoom_Architecture_DataAnnotation_FluentAPI.Models
+{
+    public class Guest
+    {
+        // Guest class representing a hotel guest with properties for ID, name, email, phone number, and associated reservations.
+        [key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; } = default!;
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Phone]
+        public string? Phone { get; set; }
+
+        public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+    }
+}
