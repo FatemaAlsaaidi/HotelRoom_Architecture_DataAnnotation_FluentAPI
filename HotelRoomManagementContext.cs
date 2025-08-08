e47c262404da8ec904260aeac225a897c9ecde18
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-09 00:07:27 +0400

    navigation property for reservations for room model

--- HotelRoomManagementContext.cs
+++ HotelRoomManagementContext.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using HotelRoom_Architecture_DataAnnotation_FluentAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.EntityFrameworkCore; // for DbContext and DbSet
+
 namespace HotelRoom_Architecture_DataAnnotation_FluentAPI
 {
-    internal class HotelRoomManagementContext
+    internal class HotelRoomManagementContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=HotelRoomManagementDB;Integrated Security=True;TrustServerCertificate=True");
+        }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+
+        // Fluent api room model
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // ----- Room -----
+            modelBuilder.Entity<Room>(b =>
+            {
+                b.HasKey("Id");
+                b.Property(r => r.RoomId)
+                    .ValueGeneratedOnAdd(); // Auto-increment primary key
+                b.Property(r => r.DailyRate)
+                    .IsRequired()
+                    .HasColumnType("decimal(10,2)")
+                    .HasPrecision(10, 2)
+                    .HasComment("Daily rate for the room in currency format");
+                b.Property(r => r.IsReserved)
+                    .IsRequired()
+                    .HasDefaultValue(false)
+                    .HasComment("Indicates if the room is currently reserved");
+                // navigation property for reservations
+                b.HasMany(r => r.Reservations)
+                 .WithOne(res => res.Room)
+                 .HasForeignKey(res => res.RoomId)
+                 .OnDelete(DeleteBehavior.Cascade);
+            });
+                
+
+           
+
+
+
     }
+
+}
+
 }
