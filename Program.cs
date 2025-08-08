6cf4360d5a15c498ac3ab3edc95d3694fcac123a
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-08 14:30:45 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelRoom_Architecture_DataAnnotation_FluentAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
