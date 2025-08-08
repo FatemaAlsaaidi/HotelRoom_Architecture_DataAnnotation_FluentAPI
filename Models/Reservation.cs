f877dd1c7fc46a82f1a3bdc761bb4780c90e87e6
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-08 15:21:52 +0400

    Reservaton class with attributes and data annotaion

---
+++ Models/Reservation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoom_Architecture_DataAnnotation_FluentAPI.Models
+{
+    public class Reservation
+    {
+        // Reservation class representing a hotel room reservation with properties for ID, guest name, number of nights, booking date, and associated room.
+        public int Id { get; set; }
+
+            [Required, StringLength(100)]
+            public string GuestName { get; set; } = default!;
+
+            [Range(1, int.MaxValue)]
+            public int Nights { get; set; }
+
+            public DateTime BookingDate { get; set; } = DateTime.Now;
+
+            // Foreign key to Room
+            public int RoomId { get; set; }
+            public Room Room { get; set; } = default!;
+        
+
+    }
+}
