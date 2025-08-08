4e5f5b867f76ba27feeb64d76ac3ef59e50a0f84
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-08 15:23:50 +0400

    put Id in reservation model as key constrain

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -9,19 +9,20 @@ namespace HotelRoom_Architecture_DataAnnotation_FluentAPI.Models
     public class Reservation
     {
         // Reservation class representing a hotel room reservation with properties for ID, guest name, number of nights, booking date, and associated room.
+        [Key]
         public int Id { get; set; }
 
-            [Required, StringLength(100)]
-            public string GuestName { get; set; } = default!;
+        [Required, StringLength(100)]
+        public string GuestName { get; set; } = default!;
 
-            [Range(1, int.MaxValue)]
-            public int Nights { get; set; }
+        [Range(1, int.MaxValue)]
+        public int Nights { get; set; }
 
-            public DateTime BookingDate { get; set; } = DateTime.Now;
+        public DateTime BookingDate { get; set; } = DateTime.Now;
 
-            // Foreign key to Room
-            public int RoomId { get; set; }
-            public Room Room { get; set; } = default!;
+        // Foreign key to Room
+        public int RoomId { get; set; }
+        public Room Room { get; set; } = default!;
         
 
     }
