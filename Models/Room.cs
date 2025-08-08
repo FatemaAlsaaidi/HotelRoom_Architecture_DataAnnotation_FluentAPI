f6705235f3eba93a58d51f75070961a2aaf7ddd6
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-08 15:19:49 +0400

    Room model attributes with data annotaion

---
+++ Models/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoom_Architecture_DataAnnotation_FluentAPI.Models
+{
+    public class Room
+    {
+        // Room class representing a hotel room with properties for ID, room number, daily rate, and reservation status.
+        [key]
+        public int Id { get; set; }
+
+        [Required]
+        public int RoomNumber { get; set; } // Unique
+
+        [Range(100, double.MaxValue)]
+        public decimal DailyRate { get; set; }
+
+        [Required]
+        public bool IsReserved { get; set; } 
+    }
+}
