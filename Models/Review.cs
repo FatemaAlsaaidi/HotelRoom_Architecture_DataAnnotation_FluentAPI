e81c3a3ea2970380842e607e766ff5a8a5e82596
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-09 00:07:56 +0400

    Make relationShip between Review and Reservation

--- Models/Review.cs
+++ Models/Review.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace HotelRoom_Architecture_DataAnnotation_FluentAPI.Models
 {
     public class Review
     {
         // Review class representing a hotel room review with properties for ID, rating, comment, and associated reservation.
+        [Key]
         public int ReviewId { get; set; }
+        [Required, Rang(1,5)]
         public int Rating { get; set; } // Rating out of 5
+
+        [Required, StringLength(500)]
         public string Comment { get; set; } = default!;
+
+        // relationShip between Review and Reservation
+        [ForeginKey("Reservation")]
+        public int ResId { get; set; } // Foreign key to Reservation
+        public Reservation Reservation { get; set; } = default!; // Navigation property to Room
+
+
     }
 }
