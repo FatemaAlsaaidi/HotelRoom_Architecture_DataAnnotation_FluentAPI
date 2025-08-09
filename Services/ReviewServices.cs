fe200d9855c44f92c23e87d33291750c3a8f60fd
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-09 14:32:49 +0400

    Assuming this is the correct namespace for IReviewRepo  + create Constructor Injection

--- Services/ReviewServices.cs
+++ Services/ReviewServices.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelRoomDB.repositories; // Assuming this is the correct namespace for IReviewRepo
+
 
 namespace HotelRoomDB.Services
 {
     public class ReviewServices
     {
+        // Constructor Injection 
+        private readonly IReviewRepo _reviewRepository;
+        public ReviewServices(IReviewRepo reviewRepository)
+        {
+            _reviewRepository = reviewRepository ?? throw new ArgumentNullException(nameof(reviewRepository));
+        }
+
+
+
     }
 }
