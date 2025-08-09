1a1143d7b16299e5fbc74ec1e4dade6522f7c45a
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-09 14:59:34 +0400

    namespace for IGuestRepo + Constructor Injection

--- Services/GuestServices.cs
+++ Services/GuestServices.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelRoomDB.repositories; // Assuming this is the correct namespace for IGuestRepo
+
 
 namespace HotelRoomDB.Services
 {
     public class GuestServices
     {
+        // Constructor Injection 
+        private readonly IGuestRepo _guestRepository;
+        public GuestServices(IGuestRepo guestRepository)
+        {
+            _guestRepository = guestRepository ?? throw new ArgumentNullException(nameof(guestRepository));
+        }
+        // Add methods for guest services here, e.g., GetGuestById, AddGuest, UpdateGuest, DeleteGuest, etc.
     }
 }
