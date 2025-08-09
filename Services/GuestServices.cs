a9dffe99725e6e2b24ffc1610d5d9e3b56672c48
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-09 19:38:46 +0400

    Add new Guest

--- Services/GuestServices.cs
+++ Services/GuestServices.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HotelRoomDB.repositories; // Assuming this is the correct namespace for IGuestRepo
+using HotelRoom_Architecture_DataAnnotation_FluentAPI.Models;
 
 
 namespace HotelRoomDB.Services
@@ -17,5 +18,21 @@ namespace HotelRoomDB.Services
             _guestRepository = guestRepository ?? throw new ArgumentNullException(nameof(guestRepository));
         }
         // Add methods for guest services here, e.g., GetGuestById, AddGuest, UpdateGuest, DeleteGuest, etc.
+        // Add new Guest
+        public void AddNewGuest(int guestId, string fname, string lname, string nationalID, string phone)
+        {
+            var guest = new Guest
+            {
+                GuestId = guestId,
+                Fname = fname,
+                Lname = lname,
+                NationalID = nationalID,
+                Phone = phone
+                
+            };
+            _guestRepository.AddGuest(guest);
+        }
+
+        public void RemoveGuest(int guestId) { }
     }
 }
