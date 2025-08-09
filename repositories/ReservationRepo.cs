802604800db268599398dbc8100992f8a8030613
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-08-09 14:19:51 +0400

    Add a new reservation to the database

--- repositories/ReservationRepo.cs
+++ repositories/ReservationRepo.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelRoom_Architecture_DataAnnotation_FluentAPI.Models;
+
 
 namespace HotelRoomDB.repositories
 {
@@ -13,6 +15,13 @@ namespace HotelRoomDB.repositories
         public ReservationRepo(HotelRoomManagementDBContext context)
         {
             _context = context;
+        }
+
+        // Add a new reservation to the database
+        public void AddReservation(Reservation reservation)
+        {
+            _context.Reservations.Add(reservation); // Adds the new reservation to the context
+            _context.SaveChanges(); // Saves changes to the database
         }
     }
 }
