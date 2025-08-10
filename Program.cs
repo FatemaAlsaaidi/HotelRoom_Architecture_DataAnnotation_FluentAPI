3dbf52f5a35963c5d78ab294e0f672fbc6a3968a
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-10 09:07:15 +0400

    Create Hold the Console +MainMenu functions

--- Program.cs
+++ Program.cs
@@ -19,6 +19,47 @@
             Console.WriteLine("Thank you for using the Hotel Room Management System. Goodbye!");
         }
 
+        // Hold the Console
+        public void HoldConsole()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        // Main Menu
+        public void MainMenu()
+        {
+            bool showMenu = true;   
+            while (showMenu)
+            Console.Clear();
+            WellcomeMessage();
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1. Sign Up");
+            Console.WriteLine("2. Login ");
+            Console.WriteLine("0. Exit");
+            char choice = Console.ReadKey().KeyChar;
+
+            switch (choice) 
+            { 
+                case '1':
+                    Console.WriteLine("\nSign Up selected.");
+                    // Call Sign Up method here
+                    break;
+                case '2':
+                    Console.WriteLine("\nLogin selected.");
+                    // Call Login method here
+                    break;
+                case '0':
+                    Console.WriteLine("\nExiting the application.");
+                    ExitMessage();
+                    HoldConsole();
+                    showMenu = false; // Exit the loop
+                    break;
+            }
+        }
+
+
 
     }
 }
