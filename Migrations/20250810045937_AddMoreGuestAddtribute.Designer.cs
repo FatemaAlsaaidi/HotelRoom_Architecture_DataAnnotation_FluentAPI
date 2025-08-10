a8479b3ecd7a8f62695c67063f00e0c89c0fd8bd
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-10 09:07:30 +0400

    new migration create

--- Migrations/20250810045937_AddMoreGuestAddtribute.Designer.cs
+++ Migrations/20250810045937_AddMoreGuestAddtribute.Designer.cs
@@ -4,6 +4,7 @@ using HotelRoom_Architecture_DataAnnotation_FluentAPI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelRoomDB.Migrations
 {
     [DbContext(typeof(HotelRoomManagementDBContext))]
-    partial class HotelRoomManagementDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250810045937_AddMoreGuestAddtribute")]
+    partial class AddMoreGuestAddtribute
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
