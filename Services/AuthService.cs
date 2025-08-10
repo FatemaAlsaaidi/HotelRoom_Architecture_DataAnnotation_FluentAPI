2a0c8d18eb5eb6ee0e034ed28973e6dd65b8beaa
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-10 09:39:35 +0400

    SignUp function

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -4,14 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelRoom_Architecture_DataAnnotation_FluentAPI.Models;
+using HotelRoomDB.Data;
 
 namespace HotelRoomDB.Services
 {
     public class AuthService
     {
         // Constructor to inject the repository
-        private readonly IGuestRepo _repo;
-        public AuthService(IGuestRepo repo) => _repo = repo;
+        private readonly IGuestServices _GuestServices;
+        private readonly IDataEntered _DataEntered;
+        public AuthService(IGuestServices GuestService) => _GuestServices = GuestService;
+        public AuthService(IDataEntered DataEntered) => _DataEntered = DataEntered;
+
+        // SignUp function 
+        public void SignUp()
+        {
+            int guestId = _DataEntered.EnterGuestId();
+            string firstName = _DataEntered.EnterGuestFirstName();
+            string lastName = _DataEntered.EnterGuestLastName();
+            string nationalId = _DataEntered.EnterGuestNationalID();
+            string phone = _DataEntered.EnterGuestPhoneNumber();
+            string password = _DataEntered.EnterPasswordForSignUp();
+            _GuestServices.AddNewGuest(guestId, firstName, lastName, nationalId, phone, password);
+        }
+
+
+
 
 
     }
