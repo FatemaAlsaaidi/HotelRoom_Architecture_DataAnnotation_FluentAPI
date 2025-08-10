dd03a3c05f8cc04150deea33b6059ade5c6f181d
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-10 08:19:13 +0400

    Wellcome Message Massage  Exit Message

--- Program.cs
+++ Program.cs
@@ -6,5 +6,19 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        // Wellcome Message Massage
+        public void WellcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Hotel Room Management System!");
+        }
+
+        // Exit Message
+        public void ExitMessage()
+        {
+            Console.WriteLine("Thank you for using the Hotel Room Management System. Goodbye!");
+        }
+
+
     }
 }
