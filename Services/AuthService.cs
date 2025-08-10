c3094c68d4722483e13f98b70be08bd479c2b672
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-10 09:16:53 +0400

    Constructor to inject the repository

---
+++ Services/AuthService.cs
@@ -0,0 +1,18 @@
+﻿using HotelRoomDB.repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoomDB.Services
+{
+    public class AuthService
+    {
+        // Constructor to inject the repository
+        private readonly IGuestRepo _repo;
+        public AuthService(IGuestRepo repo) => _repo = repo;
+
+
+    }
+}
