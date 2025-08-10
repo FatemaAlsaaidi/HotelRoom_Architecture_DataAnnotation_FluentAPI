767cc2129367ea027da8a4fa1aa46ad92d1adbfc
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-10 09:41:16 +0400

    Validates the password of a user

---
+++ Validation/PasswordVlidate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoomDB.Validation
+{
+    public class PasswordVlidate
+    {
+        public static bool ValidatePassword(string password) // Validates the password of a user
+        {
+            if (string.IsNullOrWhiteSpace(password) || password.Length ==3)
+                return false;// Check if the password is null, empty, or not equal 3 characters long
+            else return true;
+        }
+
+    }
+}
